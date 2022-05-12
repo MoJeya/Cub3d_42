2d6c4a5970d79b4f83982c49b5d51da33deacc9d
Romy Schleicher <rschleic@2-F-6.42heilbronn.de>
2022-05-12 17:11:26 +0200

    error_messages.c refactored

--- error_messages.c
+++ error_messages.c
@@ -6,90 +6,87 @@
 /*   By: rschleic <rschleic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:50:16 by rschleic          #+#    #+#             */
-/*   Updated: 2022/05/11 11:31:14 by rschleic         ###   ########.fr       */
+/*   Updated: 2022/05/12 16:59:26 by rschleic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// typedef enum free_code{
-// 	INFO_STRING,
-// 	TEXTURE_PATH,
-// 	F_C,
-// 	INFO_MAP,
-// }	t_free_code;
-
-void    error_exit(char *str, t_gen_info *info)
+void	error_exit(char *str, t_gen_info *info)
 {
-    if (info->fd != -1)
-        close(info->fd);
-    ft_putendl_fd(str, 2);
-    exit(1);
+	if (info->fd != -1)
+		close(info->fd);
+	if (info->path != NULL)
+		free(info->path);
+	ft_putendl_fd(str, 2);
+	exit(1);
 }
 
-void    error_free_exit(char *str, t_gen_info *info, int state)
+void	free_map(t_gen_info *info)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    close(info->fd);
-    if (info->path == NULL)
-        free(info->path);
-    if (state == INFO_STRING)
-    {
-        if (info->info_string)
-        {
-            while (info->info_string[i])
-            {
-                free(info->info_string[i]);
-                i++;
-            }
-            free (info->info_string);
-        }
-    }
-    else if (state == TEXTURE_PATH)
-    {
-        if (info->texture_NO_path)
-            free(info->texture_NO_path);
-        if (info->texture_SO_path)
-            free(info->texture_SO_path);
-        if (info->texture_WE_path)
-            free(info->texture_WE_path);
-        if (info->texture_EA_path)
-            free(info->texture_EA_path);
-    }
-    else if (state == INFO_MAP)
-    {
-        i = 0;
-        if (info->info_string)
-        {
-            while (info->info_string[i])
-            {
-                free(info->info_string[i]);
-                i++;
-            }
-            free (info->info_string);
-        }
-        if (info->texture_NO_path)
-            free(info->texture_NO_path);
-        if (info->texture_SO_path)
-            free(info->texture_SO_path);
-        if (info->texture_WE_path)
-            free(info->texture_WE_path);
-        if (info->texture_EA_path)
-            free(info->texture_EA_path);
-        i = 0;
-        if (info->map)
-        {
-            while (info->map[i])
-            {
-                free(info->map[i]);
-                i++;
-            }
-            free (info->map);
-        }
-    }
-    ft_putendl_fd(str, 2);
-    exit(1);   
+	i = 0;
+	if (info->map)
+	{
+		while (info->map[i])
+		{
+			free(info->map[i]);
+			i++;
+		}
+		free (info->map);
+	}
 }
-//merge
+
+void	free_infostring(t_gen_info *info)
+{
+	int	i;
+
+	i = 0;
+	if (info->info_string)
+	{
+		while (info->info_string[i])
+		{
+			free(info->info_string[i]);
+			i++;
+		}
+		free (info->info_string);
+	}
+}
+
+void	free_texturepath(t_gen_info *info)
+{
+	if (info->texture_NO_path)
+		free(info->texture_NO_path);
+	if (info->texture_SO_path)
+		free(info->texture_SO_path);
+	if (info->texture_WE_path)
+		free(info->texture_WE_path);
+	if (info->texture_EA_path)
+		free(info->texture_EA_path);
+}
+
+void	error_free_exit(char *str, t_gen_info *info, int state)
+{
+	int	i;
+
+	i = 0;
+	close(info->fd);
+	if (info->path != NULL)
+		free(info->path);
+	if (state == INFO_STRING)
+		free_infostring(info);
+	else if (state == TEXTURE_PATH)
+	{
+		free_infostring(info);
+		free_texturepath(info);
+	}
+	else if (state == INFO_MAP)
+	{
+		free_infostring(info);
+		free_texturepath(info);
+		free_map(info);
+	}
+	ft_putendl_fd(str, 2);
+	exit(1);
+}
