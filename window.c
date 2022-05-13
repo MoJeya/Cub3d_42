3d74c75927b0f7027cfbc934579014c9f9562ce8
Romy Schleicher <rschleic@2-A-2.42heilbronn.de>
2022-05-13 18:06:41 +0200

    die Mauer muss hin

--- window.c
+++ window.c
@@ -6,7 +6,7 @@
 /*   By: rschleic <rschleic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:42:57 by mjeyavat          #+#    #+#             */
-/*   Updated: 2022/05/09 09:42:35 by rschleic         ###   ########.fr       */
+/*   Updated: 2022/05/13 18:04:48 by rschleic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ void creat_tile(mlx_image_t *tile)
 
 	x = 0;
 	y = 0;
+	// (void)tile;
 	while (x < TILES_W)
 	{
 		y = 0;
@@ -47,26 +48,34 @@ void creat_tile(mlx_image_t *tile)
 	}
 }
 
-void creat_map(mlx_t *mlx, mlx_image_t *img, t_gen_info *info)
+void creat_map(mlx_t *mlx, t_gen_info *info, mlx_image_t *tiles)
 {
 	int x;
 	int y;
+	int	right;
+	int	down;
 
-	x = 0;
+	// x = 0;
 	y = 0;
+	// (void)mlx;
+	right = 0;
+	down = y;
 	while (y < info->map_y)
 	{
-		x = 0;
-		while(x < info->map_x)
+		x = -1;
+		while(x <= info->map_x)
 		{
 			if (info->map[y][x] == '1')
 			{
-				creat_tile(img);
-				mlx_image_to_window(mlx, img, y+25, x+25);
+				creat_tile(tiles);
+				mlx_image_to_window(mlx, tiles, right, down);
+				// mlx_image_to_window(mlx, img, y+25, x+25);
 			}
 			x++;
+			right += 25;
 		}
 		y++;
+		down += 25;
 	}
 }
 
@@ -74,20 +83,21 @@ int32_t	create_window(mlx_t *mlx, mlx_image_t *panel, t_gen_info *info)
 {
 	mlx_image_t *tiles;
 
+	(void)panel;
 	tiles = NULL;
-	mlx = mlx_init(info->win_x, info->win_y, "CUBE3D", true);
+	mlx = mlx_init(info->win_x, info->win_y, "CUBE3D", true);//blank window
 	if (!mlx)
 		exit(EXIT_FAILURE);
-	panel = mlx_new_image(mlx, info->win_x, info->win_y);//dass canvas für denn pixel
-	tiles = mlx_new_image(mlx, TILES_W, TILES_H);
-	memset(panel->pixels, 0, panel->width * panel->height * sizeof(int));
+	// panel = mlx_new_image(mlx, info->win_x, info->win_y);//dass canvas für denn pixel in der grösse vom window - noch unsichtbar
+	tiles = mlx_new_image(mlx, TILES_W, TILES_H);//canvas in der grösse von 25x25
+	// memset(panel->pixels, 200, panel->width * panel->height * sizeof(int));//panel sichtbar machen, sozusagen eine pixel put
 	// memset(tiles->pixels, 250, tiles->width * tiles->height * sizeof(int));
-	creat_map(mlx, tiles, info);
+	creat_map(mlx, info, tiles);
 	// creat_tile(tiles);
 	// printf("%d\n", tiles->width);
-	mlx_image_to_window(mlx, panel, 0, 0); //adds render quoe
-	mlx_image_to_window(mlx, tiles, 0, 0);
-	// mlx_image_to_window(mlx, tiles, 36, 0);
+	// mlx_image_to_window(mlx, panel, 0, 0); //adds render quoe//zieht das panel auf das window
+	// mlx_image_to_window(mlx, tiles, 0, 0);
+	// mlx_image_to_window(mlx, tiles, 25, 0);
 	// mlx_put_pixel(tiles, WINDOW_X/2, WINDOW_Y/2, 0xFFFFFFFF);
 	// mlx_delete_image(mlx, tiles);
 	// mlx_image_to_window(mlx, g_img, 0, 0);
@@ -96,4 +106,8 @@ int32_t	create_window(mlx_t *mlx, mlx_image_t *panel, t_gen_info *info)
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
-
+//wir brauchen gar kein panel auf das wir extra zeichnen, die tiles reichen
+// ohne mlx_image_to_window(mlx, tiles, 36, 0) gibt es nur die komische form von create tile()
+// mit mlx_image_to_window(mlx, tiles, 36, 0) gibt es ein 25x25 tile
+//so without create_map kein tile
+//unser window ist eins zu lang in x richtung
