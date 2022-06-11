bb2e8db4ebcdfe0ff83dad59dde14dd47300f818
Moghanavathanan Jeyavathanan <mjeyavat@3-F-2.42heilbronn.de>
2022-06-11 20:32:39 +0200

    mapper bool segfault fixed

--- parser_stuff/init_data.c
+++ parser_stuff/init_data.c
@@ -6,7 +6,7 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 18:30:36 by rschleic          #+#    #+#             */
-/*   Updated: 2022/06/11 19:19:14 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2022/06/11 20:30:05 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,8 @@ int	parse_data_info(t_gen_info *info)
 		}
 		i++;
 	}
-	printf("i: %d\n", i);
 	i = 0;
+	printf("i: %d\n", i);
 	while (info->info_string[i][0] != '\0')
 	{
 		if (!parse_color_settings(info->info_string[i], info))
@@ -62,7 +62,8 @@ int	parse_data_info(t_gen_info *info)
 				info->map_x = get_max_len(&info->info_string[i], info);
 				if (t_b_check(&info->info_string[i], info) == 0)
 					break ;
-			}		
+				mapper = true;
+			}	
 			else
 				break ;
 			init_map(info, &info->info_string[i]);
@@ -72,8 +73,11 @@ int	parse_data_info(t_gen_info *info)
 	}
 	if (!info->floor.set || !info->ceiling.set)
 		error_free_exit("ERROR\ncolor setting is missing", info, INFO_MAP);
-	if (map_base_player_check(info))
-		return (1);
+	if (mapper == true)
+	{
+		if (map_base_player_check(info))
+			return (1);	
+	}
 	info->map = NULL;
 	error_free_exit("\033[31mMAP IS NOT VALID\033[0m", info, INFO_MAP);
 	return (0);
